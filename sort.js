d6f74dbe1902c5ee664ddefcdf4c98cdb99483e5
Michal Huzevka <michal.huzevka583@gmail.com>
2019-11-28 13:54:59 +1100

    Do deep sort instead of shallow sort. Allow sort.js to be run without any arguments

--- sort.js
+++ sort.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const args = process.argv.slice(2);
+const { execSync } = require('child_process');
 
 /*
 Usage:
@@ -8,19 +9,48 @@ node sort.js {arguments}
 
 The script updates each file by sorting the labels by their keys.
 
+If no arguments are passed, all language files will be sorted.
+
 Example:
 node sort.js languages/de-DE/assess-api.json languages/de-DE/questions-api.json
+node sort.js
 */
-args.forEach((arg) => {
-    const text = fs.readFileSync(arg);
-    const parsedJson = JSON.parse(text);
-    const sortedKeys = Object.keys(parsedJson).sort();
+let files;
+
+if (args.length) {
+    files = args;
+} else {
+    files = execSync("find ./languages -type f -name '*.json'", {
+        encoding: 'utf8'
+    }).trim().split(/[\r\n]+/gm);
+}
+
+files.forEach((file) => {
+    const text = fs.readFileSync(file);
+    let result = JSON.parse(text);
+    
+    result = recursiveSort(result);
+    result = JSON.stringify(result, null,'    ');
+    fs.writeFileSync(file, result); 
+});
+
+function recursiveSort(obj) {
+    const sortedKeys = Object.keys(obj).sort();
     let result = {};
     
     sortedKeys.forEach((key) => {
-        result[key] = parsedJson[key];
-    });          
-    
-    result = JSON.stringify(result, null,'    ');
-    fs.writeFileSync(arg, result); 
-});
+        const val = obj[key];
+
+        // Do not sort arrays, as the order of elements is important.
+        // E.g. reordering questions-api.json's characterMapCharacters would
+        // cause the special characters to appear in a different order in
+        // the product.
+        if (val && typeof val === 'object' && !Array.isArray(val)) {
+            result[key] = recursiveSort(val);
+        } else {
+            result[key] = val;
+        }
+    });
+
+    return result;
+}
